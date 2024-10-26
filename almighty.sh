58c6c8b960cf4531d64f4e134b69835e9fd3d4b9
Dhane Ashley Diabajo <dhaneashleyd@gmail.com>
2024-10-27 00:05:35 +0800

    Update almighty.sh

--- almighty.sh
+++ almighty.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 CYAN='\033[1;36m'
 RESET='\033[0m'
 GREEN='\033[0;32m'
@@ -10,6 +12,8 @@ echo -e """${CYAN}
 ▒█▄▄█ █░░ █░▀░█ ▀█▀ █░▀█ █▀▀█ ░░█░░ █▄▄█ 
 ▒█░▒█ ▀▀▀ ▀░░░▀ ▀▀▀ ▀▀▀▀ ▀░░▀ ░░▀░░ ▄▄▄█${RESET}"""
 
+echo -e "${GREEN}By Dhane Ashley Diabajo${RESET}"
+echo ""
 usage() {
     echo "Usage: $0 -d <target_domain>"
     exit 1
@@ -31,7 +35,7 @@ mkdir -p Subdomains
 echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Subfinder, Anew, and Httpx...${RESET}"
-echo ${TARGET_DOMAIN} | subfinder -silent | anew | httpx -silent >> Subdomains/subdomains.txt
+echo ${TARGET_DOMAIN} | subfinder -recursive -active -silent | anew | httpx -silent >> Subdomains/subdomains.txt
 echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Httpx Scanning for sensitive files...${RESET}"
@@ -46,7 +50,7 @@ cat Subdomains/subdomains.txt | httpx -silent -sc -title -cl --tech-detect >> Su
 echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Naabu...${RESET}"
-cat Subdomains/subdomains.txt | naabu --passive -silent > Subdomains/ports.txt
+cat Subdomains/subdomains.txt | naabu -v --passive -silent > Subdomains/ports.txt
 echo -e "${CYAN}Task Completed!${RESET}"
 
 echo -e "${GREEN}Running Subzy and checking for subdomain takeovers...${RESET}"
@@ -78,69 +82,64 @@ for year in {2000..2024}; do
     cat Subdomains/subdomains.txt | nuclei -silent -rate-limit 200 -t /$HOME/nuclei-templates/http/cves/$year/*.yaml > Subdomains/nuclei/nuclei-$year.txt
 done
 
-source /$HOME/bugbounty450/venv/bin/activate
+source /$HOME/venv/bin/activate
 
 mkdir -p urls
 
 echo -e "${GREEN}Running Katana${RESET}"
 echo "${TARGET_DOMAIN}" | katana -silent -d 5 -ps -pss waybackarchive,commoncrawl,alienvault > urls/katana.txt
-cat urls/katana.txt | uro > urls/katana-clean.txt
 echo -e "${GREEN}Task finished${RESET}"
 
 echo -e "${GREEN}Running GAU${RESET}"
 echo "${TARGET_DOMAIN}" | gau --subs > urls/gau.txt
-cat urls/gau.txt | uro > urls/gau-clean.txt
 echo -e "${GREEN}Task finished${RESET}"
 
 echo -e "${GREEN}Running Waybackurls...${RESET}"
 waybackurls "${TARGET_DOMAIN}" > urls/waybackurls.txt
-cat urls/waybackurls.txt | uro > urls/waybackurls-clean.txt
 echo -e "${GREEN}Task finished${RESET}"
 
 echo -e "${GREEN}Combining all URLs${RESET}"
-cat urls/katana-clean.txt urls/gau-clean.txt urls/waybackurls-clean.txt > urls/final-clean.txt
-rm urls/katana-clean.txt urls/gau-clean.txt urls/waybackurls-clean.txt
+cat urls/katana.txt urls/gau.txt urls/waybackurls.txt > urls/final-clean.txt
 echo -e "${GREEN}Task Completed${RESET}"
 
 echo -e "${GREEN}Checking all js files and running linkfinder...${RESET}"
 mkdir -p js-files
 linkfinder_path="/$HOME/LinkFinder/linkfinder.py"
-output_file="js-files/endpoints.txt"
-> "$output_file"
 
-cat urls/final-clean.txt | grep "\.js$" | uro | while read -r url; do
+cat urls/final-clean.txt | grep "\.js$" | while read -r url; do
     output=$(python3 "$linkfinder_path" -i "$url" -o cli 2>/dev/null)
-    if [[ "$output" != *"Usage: python /home/kali/LinkFinder/linkfinder.py [Options] use -h for help"* ]] && \
-       [[ "$output" != *"Error: invalid input defined or SSL error: HTTP Error 404: Not Found"* ]] && \
-       [[ ! -z "$output" ]]; then
-        echo -e "\e[32m$url\e[0m"
-        echo "$output"
-        echo ""
-        echo "$url" >> "$output_file"
-        echo "$output" >> "$output_file"
-        echo "" >> "$output_file"
+
+    # Append to js-files/endpoints.txt if output is not empty and without specific error messages
+    if [[ "$output" != *"Usage: python"* ]] && [[ "$output" != *"HTTP Error 404: Not Found"* ]] && [[ -n "$output" ]]; then
+        {
+            echo "$url"
+            echo "$output"
+            echo ""  # Add an extra space after each target host
+        } >> js-files/endpoints.txt
     fi
 done
 echo -e "${GREEN}Task Completed${RESET}"
 
+
 echo -e "${GREEN}Checking for possible XSS vulnerabilities...${RESET}"
-cat urls/final-clean.txt | gf xss | uro | qsreplace '"><img src=x onerror=alert("XSS")>' | 
+cat urls/final-clean.txt | gf xss | qsreplace '"><img src=x onerror=alert("XSS")>' | 
 while read -r host; do 
     if curl -sk --path-as-is "$host" | grep -qs '"><img src=x onerror=alert("XSS")>'; then 
         echo "$host is vulnerable" >> urls/possible-xss.txt
     fi
 done
 
-cat urls/final-clean.txt | gf sqli | uro > urls/sqli.txt
-cat urls/final-clean.txt | gf redirect | uro | egrep -iv "wp-" > urls/open-redirect.txt
-cat urls/final-clean.txt | gf ssrf | uro > urls/ssrf.txt
-cat urls/final-clean.txt | gf rce | uro > urls/rce.txt
-cat urls/final-clean.txt | gf lfi | uro > urls/lfi.txt
-cat urls/final-clean.txt | gf interestingEXT | uro > urls/interesting-extentions.txt
-cat urls/final-clean.txt | gf interestingparams | uro > urls/interesting-params.txt
-cat urls/final-clean.txt | gf debug_logic | uro > urls/debug-logic.txt
-cat urls/final-clean.txt | gf img-traversal | uro > urls/img-traversal.txt
-cat urls/final-clean.txt | gf ssti | uro > urls/ssti.txt
+cat urls/final-clean.txt | gf sqli > urls/sqli.txt
+cat urls/final-clean.txt | gf redirect | egrep -iv "wp-" > urls/open-redirect.txt
+cat urls/final-clean.txt | gf ssrf > urls/ssrf.txt
+cat urls/final-clean.txt | gf rce > urls/rce.txt
+cat urls/final-clean.txt | gf lfi > urls/lfi.txt
+cat urls/final-clean.txt | gf interestingEXT > urls/interesting-extentions.txt
+cat urls/final-clean.txt | gf interestingparams > urls/interesting-params.txt
+cat urls/final-clean.txt | gf debug_logic > urls/debug-logic.txt
+cat urls/final-clean.txt | gf img-traversal > urls/img-traversal.txt
+cat urls/final-clean.txt | gf ssti > urls/ssti.txt
+
 
 mkdir urls/nuclei
 for i in {2000..2024}; do
